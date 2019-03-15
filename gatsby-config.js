8ce750658a4e9c4bc010ec791c851c31929e5f57
Andy Cunningham <andycunn@gmail.com>
2019-03-14 22:15:40 -0700

    add segment

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,6 +82,25 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-segment-js`,
+      options: {
+        // your segment write key for your production environment
+        // when process.env.NODE_ENV === 'production'
+        // required; non-empty string
+        prodKey: `w8DpBL4CbIx1ISHBP3DOQqCFVV3S9gbA`,
+
+        // if you have a development env for your segment account, paste that key here
+        // when process.env.NODE_ENV === 'development'
+        // optional; non-empty string
+        devKey: `SEGMENT_DEV_WRITE_KEY`,
+
+        // boolean (defaults to false) on whether you want
+        // to include analytics.page() automatically
+        // if false, see below on how to track pageviews manually
+        trackPage: true,
+      },
+    },
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
